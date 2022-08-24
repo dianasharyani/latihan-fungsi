89cc9eea23c6bd9c749aabfe581d941420d33198
dianasharyani <109190806+dianasharyani@users.noreply.github.com>
2022-08-24 14:54:37 +0700

    Add files via upload

---
+++ fungsibalok_dian.php
@@ -0,0 +1,9 @@
+<?php
+$p = 18;
+$l = 10;
+$t = 12;
+function Volumebalok($p,$l,$t) {
+return ($p*$l*$t);
+}
+echo "Volume balok tersebut adalah =".Volumebalok($p,$l,$t)."cm";
+?>
---
+++ fungsikerucut_dian.php
@@ -0,0 +1,9 @@
+<?php
+$phi = 22/7;
+$r = 28;
+$t = 40;
+function VolumeKerucut($phi,$r,$t) {
+return (1/3*$phi*$r*$r*$t);
+}
+echo "Volume kerucut tersebut adalah =".Volumekerucut($phi,$r,$t)."cm";
+?>
